51824d1622df00805f02a5e2bcb4915d73aa47ed
it63070217 <63070217@kmitl.ac.th>
2022-04-08 17:18:54 +0700

    123

--- bisad/cart.php
+++ bisad/cart.php
@@ -184,13 +184,16 @@
                 echo "<div class=\"col-2\" align=\"right\"> จำนวน" . " ";
                 echo "<a href=\"#\" type=\"button\" class=\"btn btn-outline-primary btn-sm\">-</a> " . $row['quantities'] . " ";
                 echo "<a href=\"#\" type=\"button\" class=\"btn btn-outline-primary btn-sm\">+</a> ชิ้น</div>";
-                echo "<div class=\"col-1\"><a href=\"#\" type=\"button\" class=\"btn btn-outline-danger btn-sm\">ลบ</a></div>";
+                echo "<div class=\"col-1\"><button type=\"button\" class=\"btn btn-outline-danger btn-sm\" onclick=\"delete()\">ลบ</a></div>";
                 echo "<div class=\"col-2\"></div>";
                 echo "</div><br><br>";
   
             }
         }
-        
+
+        function delete(){
+            $sql = "DELETE FROM cart WHERE product_id = " . $row['product_id'];
+        }
 
         echo "<div class=\"row\">";
         echo "<div class=\"col-2\"></div>";

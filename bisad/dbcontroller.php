8779286b2bcd6d925aa1bf592ca69f059b538e13
mmmmaill <63070250@kmitl.ac.th>
2022-04-11 22:10:31 +0700

    กกก

--- bisad/dbcontroller.php
+++ bisad/dbcontroller.php
@@ -3,7 +3,7 @@ class DBController {
 	private $host = "localhost";
 	private $user = "root";
 	private $password = "";
-	private $database = "storetest";
+	private $database = "store";
 	private $conn;
 	
 	function __construct() {

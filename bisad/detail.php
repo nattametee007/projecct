27472921427683386ac14e1d066320f274d222c5
mmmmaill <63070250@kmitl.ac.th>
2022-04-15 14:29:21 +0700

    ggg

--- bisad/detail.php
+++ bisad/detail.php
@@ -16,97 +16,95 @@ $db_handle = new DBController();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
-    body {
-        font-family: Arial;
-    }
+        body {
+            font-family: Arial;
+        }
 
-    * {
-        box-sizing: border-box;
-    }
+        * {
+            box-sizing: border-box;
+        }
 
-    form.example input[type=text] {
-        padding: 10px;
-        font-size: 17px;
-        border: 1px solid grey;
-        float: left;
-        width: 80%;
-        background: #f1f1f1;
-    }
+        form.example input[type=text] {
+            padding: 10px;
+            font-size: 17px;
+            border: 1px solid grey;
+            float: left;
+            width: 80%;
+            background: #f1f1f1;
+        }
 
-    form.example button {
-        float: left;
-        width: 20%;
-        padding: 10px;
-        background: #2196F3;
-        color: white;
-        font-size: 17px;
-        border: 1px solid grey;
-        border-left: none;
-        cursor: pointer;
-    }
+        form.example button {
+            float: left;
+            width: 20%;
+            padding: 10px;
+            background: #2196F3;
+            color: white;
+            font-size: 17px;
+            border: 1px solid grey;
+            border-left: none;
+            cursor: pointer;
+        }
 
-    form.example button:hover {
-        background: #0b7dda;
-    }
+        form.example button:hover {
+            background: #0b7dda;
+        }
 
-    form.example::after {
-        content: "";
-        clear: both;
-        display: table;
-    }
+        form.example::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
 
-    .dropbtn {
-        background-color: #4CAF50;
-        color: white;
-        padding: 16px;
-        font-size: 16px;
-        border: none;
-        cursor: pointer;
-    }
+        .dropbtn {
+            background-color: #4CAF50;
+            color: white;
+            padding: 16px;
+            font-size: 16px;
+            border: none;
+            cursor: pointer;
+        }
 
-    .dropdown {
-        position: relative;
-        display: inline-block;
-    }
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
 
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        right: 0;
-        background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-        z-index: 1;
-    }
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            right: 0;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
 
-    .dropdown-content a {
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-    }
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
 
-    .dropdown-content a:hover {
-        background-color: #f1f1f1;
-    }
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
 
-    .dropdown:hover .dropdown-content {
-        display: block;
-    }
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
 
-    .dropdown:hover .dropbtn {
-        background-color: #3e8e41;
-    }
+        .dropdown:hover .dropbtn {
+            background-color: #3e8e41;
+        }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="user.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                    fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
-                    <path
-                        d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+            <a class="navbar-brand" href="user.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
                 </svg></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
@@ -120,17 +118,13 @@ $db_handle = new DBController();
                 </form>
 
 
-                <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                        fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                        <path
-                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                     </svg></a>
 
                 <div class="dropdown" style="float:right;">
-                    <button class="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                            <path
-                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                    <button class="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                         </svg></button>
                     <div class="dropdown-content">
                         <a href="index.php">สมัครสมาชิก</a>
@@ -145,7 +139,6 @@ $db_handle = new DBController();
     </nav>
     <?php
     $product = $_GET['product'];
-    //$product = mysqli_real_escape_string($conn,$_GET['product']);
     $user = $_GET['user'];
     ?>
     <center>
@@ -154,12 +147,12 @@ $db_handle = new DBController();
 
                 <div class="col-sm-5  ">
                     <?php
-                    
+
                     $product_array = $db_handle->runQuery("SELECT * FROM inventory WHERE product_id=$product");
                     if (!empty($product_array)) {
                         foreach ($product_array as $key => $value) {
                     ?>
-                    <img class="product-image img-fluid" src="picture/<?php echo $product_array[$key]["picture"]; ?>">
+                            <img class="product-image img-fluid" src="picture/<?php echo $product_array[$key]["picture"]; ?>">
 
                     <?php
                         }
@@ -171,36 +164,63 @@ $db_handle = new DBController();
                 <div class="col-sm-7 ">
 
                     <div class="container">
-                        <h1>
-                            <?php echo $product_array[$key]["product_name"]; ?><br>
-                            <?php echo $product_array[$key]["cost"]; ?><br>
-                            <?php echo $product_array[$key]["detail"]; ?>
-                        </h1>
+                        <h1><?php echo $product_array[$key]["product_name"]; ?></h1><br>
+                        <h3> <?php echo $product_array[$key]["cost"]; ?></h3><br>
+                        <h5><?php echo $product_array[$key]["detail"]; ?></h5>
+
                     </div>
                     <hr>
 
+                    <form method="POST">
+                        <div class="container">
+                            <div class="form-group">
+                                <label>สี : </label>
+                                <input type="text" class="form-control" placeholder="สี" />
+                            </div>
 
-                    <div class="container row">
+                            <div class="form-group">
+                                <label>ขนาด : </label>
+                                <input type="text" class="form-control" placeholder="ขนาด" />
+                            </div>
+                            <div class="form-group">
+                                <label>จำนวน : </label>
+                                <input type="text" class="form-control" placeholder="จำนวน" name="iqty" />
+                            </div>
 
-                        <div class="container col">
-                            <button type="button" class="container btn btn-outline-info">
-                                <a href="payment.php?product='<?php echo $product_array[$key]["product_id"]; ?>'&user='<?php echo $customer; ?>'"><h2>สั่งซื้อสินค้า</h2></a>
-                        </button></div>
 
-
-                        <div class="container col">
-                            <button type="button" class="container btn btn-outline-info">
-                                <h2>เพิ่มลงตะกร้า</h2>
-                            </button>
                         </div>
 
-                    </div>
+                        <div class="container row">
+
+                            <div class="container col">
+                                <a class="container btn btn-outline-info" href="payment.php?product='<?php echo $product_array[$key]["product_id"]; ?>'&user='<?php echo $user; ?>'">
+                                    สั่งซื้อสินค้า
+                                </a>
+
+                            </div>
+                            <div class="container col">
+                                <input type="submit" value="เพิ่มลงตะกร้า" class="container btn btn-outline-info" name="btn">
+
+                            </div>
+
+                        </div>
+                    </form>
                 </div>
 
 
             </div>
         </div>
     </center>
+    <?php
+    if (isset($_POST["btn"])) {
+
+        include("connect.php");
+        $item_qty = $_POST['iqty'];
+        $sql = "INSERT INTO cart(user_id, product_id, quantities) VALUES ($user, $product, $item_qty)";
+        mysqli_query($conn, $sql);
+    };
+
+    ?>
 </body>
 
 </html>

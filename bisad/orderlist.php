e3ef17c79565d5e1ab40e3d802aa0dd4ab61fb08
IyadaSangchan <63070251@kmitl.ac.th>
2022-04-28 10:22:13 +0700

    azsxdc

--- bisad/orderlist.php
+++ bisad/orderlist.php
@@ -126,12 +126,10 @@ $db_handle = new DBController();
 
 <body>
 
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-sm" style="background-color: #A65353;">
         <div class="container-fluid">
-            <a class="navbar-brand" href="user.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                    fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
-                    <path
-                        d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+            <a class="navbar-brand" href="user.php"><svg xmlns="http://www.w3.org/2000/svg" style='color: white;' width="25" height="25" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
                 </svg></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
@@ -139,23 +137,19 @@ $db_handle = new DBController();
 
             <ul class="navbar-nav">
 
-                <form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+                <form class="example" method="POST" action="search.php" style="margin:auto;max-width:300px">
                     <input type="text" placeholder="Search.." name="search2">
-                    <button type="submit"><i class="fa fa-search"></i></button>
+                    <button type="submit" name="btn1" style='background-color: #D99B84;'><i class="fa fa-search"></i></button>
                 </form>
 
 
-                <a class="nav-link" href="cart.php?usercart=<?= $customer ?>"><svg xmlns="http://www.w3.org/2000/svg"
-                        width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                        <path
-                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                <a class="nav-link" href="cart.php?usercart=<?= $customer ?>"><svg xmlns="http://www.w3.org/2000/svg" style='color: white;' width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                     </svg></a>
 
                 <div class="dropdown" style="float:right;">
-                    <button class="dropbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                            <path
-                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                    <button class="dropbtn" style='background-color: #D99B84;'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                         </svg></button>
                     <div class="dropdown-content">
                         <a href="index.php">สมัครสมาชิก</a>
@@ -180,14 +174,15 @@ $db_handle = new DBController();
     ?>
     <div class="container-fluid mt-4">
         <div class="container row">
-            <div class="container col-2  ">
+            <div class="container col-3  " style='margin-right:-70px'>
 
                 <ul class="nav nav-pills" role="tablist">
 
                     <li class="nav-item">
-                        <a class="nav-link active" data-bs-toggle="pill" href="#allorderid">
+                        <a class="nav-link active" data-bs-toggle="pill" style='background-color: #D99B84; color: #000000; padding-left:29px; padding-right:29px;' href="#allorderid">
                             <h6>หมายเลขคำสั่งซื้อทั้งหมด</h6>
                         </a>
+                        <br>
                     </li>
 
                     <li class="nav-item">
@@ -195,9 +190,10 @@ $db_handle = new DBController();
                     foreach ($all as $value) { ?>
                         
 
-                            <a class="nav-link" data-bs-toggle="pill" href="#orderid<?php echo $value; ?>">
+                            <a class="nav-link" style='background-color: #D99B84; color: #000000;' data-bs-toggle="pill" href="#orderid<?php echo $value; ?>">
                                 <?php echo $value; ?>
                             </a>
+                            <br>
 
                         
                         <?php } ?>
@@ -206,19 +202,19 @@ $db_handle = new DBController();
                 
             </div>
 
-            <div class="container col-10  tab-content  ">
+            <div class="container col-9  tab-content  ">
                 <div id="allorderid" class="container tab-pane active ">
                 <?php
 
 foreach ($all as $value3) { ?>
 
-        <div class="product-image col-sm-12 card">
+        <div class="product-image col-sm-12 card" style='background-color: #F2E5D5;'>
         
             <?php $queryuserr = $db_handle->runQuery("SELECT DISTINCT u.name, u.address FROM users as u INNER JOIN sale_order as so WHERE u.user_id = '$customer'");
             $querytdd = $db_handle->runQuery("SELECT DISTINCT time_date FROM sale_order WHERE order_id = '$value3'");
             $product_arrayy = $db_handle->runQuery("SELECT * FROM sale_order as so INNER JOIN inventory as i ON so.product_id = i.product_id WHERE so.order_id = '$value3'");
 
-            echo '<div style=margin-left:15%;margin-top:2%>';
+            echo '<div style=margin-left:13%;margin-top:2%>';
             echo '<h5>หมายเลขคำสั่งซื้อ : ' . $value3 . '</h5>';
             foreach ($querytdd as $key4 => $value4) {
                 echo '<h5>วัน เวลาที่สั่งซื้อ : ' . $querytdd[$key4]['time_date'] . '</h5>';
@@ -281,7 +277,7 @@ foreach ($all as $value3) { ?>
 
 
                 <div class="container tab-pane fade" id='orderid<?php echo $value; ?>'>
-                    <div class="product-image col-sm-12 card">
+                    <div class="product-image col-sm-12 card" style='background-color: #F2E5D5;'>
                     
                         <?php $queryuser = $db_handle->runQuery("SELECT DISTINCT u.name, u.address FROM users as u INNER JOIN sale_order as so WHERE u.user_id = '$customer'");
                         $querytd = $db_handle->runQuery("SELECT DISTINCT time_date FROM sale_order WHERE order_id = '$value'");

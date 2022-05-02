063cef21c993291d42d5028b7f157e5f626c0fe4
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2022-05-02 09:35:25 +0700

    Update products.php

--- bisad/products.php
+++ bisad/products.php
@@ -205,26 +205,7 @@ if (isset($_POST['btn'])) {
         </form>
     </div>
   
-    <?php
-        if(isset($_POST["btn"])) {
-            include("connect.php");
-            $iname=$_POST['iname'];
-            $ipic=$_POST['ipic'];
-            $isize=$_POST['isize'];
-            $icolour=$_POST['icolour'];
-            $icost=$_POST['icost'];
-            $idetail=$_POST['idetail'];
-            $icate=$_POST['icate'];
-
-            // SQL script for adding an item
-            $sql = "INSERT INTO inventory (product_name,picture,size,colour,cost,detail,category) VALUES ('$iname','$ipic','$isize','$icolour','$icost','$idetail','$icate')";
-      
-  
-            mysqli_query($conn,$sql);
-            header("location:products.php");
-        }
-          
-    ?>
+   
     <div class="container mt-5">
 
         <!-- top -->

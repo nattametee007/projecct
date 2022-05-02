29d1ff1778b1fc904082da364cc43ed6a15dc609
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2022-05-02 11:20:30 +0700

    Update index.php

--- bisad/index.php
+++ bisad/index.php
@@ -9,7 +9,7 @@ require_once 'config/db.php';
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
+    <title>Sign up</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">

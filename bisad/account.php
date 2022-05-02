523050c2c1f90d576587286940c81c4e4cdd1793
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2022-05-02 14:45:02 +0700

    Update account.php

--- bisad/account.php
+++ bisad/account.php
@@ -22,7 +22,7 @@ include("condb.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Page</title>
+    <title>Account Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">

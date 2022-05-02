51102079ccd31497915dce0b8f9606150268c62b
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2022-05-02 14:37:52 +0700

    Update ordercartdelete.php

--- bisad/ordercartdelete.php
+++ bisad/ordercartdelete.php
@@ -176,7 +176,7 @@ $db_handle = new DBController();
 
                 <form class="example" method="POST" action="search.php" style="margin:auto;max-width:300px">
                     <input type="text" placeholder="ค้นหา.." name="search2">
-                    <button type="submit" name="btn1" style='background-color: #D99B84;'><i class="fa fa-search"></i></button>
+                    <button type="submit" name="btn1" style='background-color: #D99B84;padding-bottom:15px; padding-top:13.5px;'><i class="fa fa-search"></i></button>
                 </form>
 
 

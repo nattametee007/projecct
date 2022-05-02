6a0a51ebd7afa10a90d29ec88d22289bd97e59bc
mmmmaill <73694268+mmmmaill@users.noreply.github.com>
2022-05-02 11:58:06 +0700

    Update detail.php

--- bisad/detail.php
+++ bisad/detail.php
@@ -116,7 +116,7 @@ $user = $_GET['user'];
 
                 <form class="example" method="POST" action="search.php" style="margin:auto;max-width:300px">
                     <input type="text" placeholder="ค้นหา.." name="search2">
-                    <button type="submit" name="btn1" style='background-color: #D99B84;'><i class="fa fa-search"></i></button>
+                    <button type="submit" name="btn1" style='background-color: #D99B84;padding-bottom:15px; padding-top:13.5px;'><i class="fa fa-search"></i></button>
                 </form>
 
 
